d87bc4c74925a3962e9d7e3db28399ad71b336cb
abdulkadirkraa <karaabdulkadir498@gmail.com>
2025-01-01 19:59:30 +0300

    refactor

--- app/src/main/java/com/abdulkadirkara/filmverse/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/abdulkadirkara/filmverse/presentation/navigation/Navigation.kt
@@ -1,19 +1,22 @@
 package com.abdulkadirkara.filmverse.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.abdulkadirkara.filmverse.presentation.screens.screencard.ScreenCard
 import com.abdulkadirkara.filmverse.presentation.screens.screendetail.ScreenDetail
 import com.abdulkadirkara.filmverse.presentation.screens.screenhome.ScreenHome
+import com.abdulkadirkara.filmverse.presentation.screens.screenhome.ScreenHomeViewModel
 
 @Composable
 fun Navigation(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screens.ScreenHome.route){
         composable(Screens.ScreenHome.route){
-            ScreenHome()
+            val viewModel: ScreenHomeViewModel = hiltViewModel()
+            ScreenHome(viewModel)
         }
         composable(Screens.ScreenDetail.route) {
             ScreenDetail()
