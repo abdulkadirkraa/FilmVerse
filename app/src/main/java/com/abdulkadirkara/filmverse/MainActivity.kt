a62d8626b264b321f224d4af6433d013f3d8bf2d
abdulkadirkraa <karaabdulkadir498@gmail.com>
2025-01-01 20:01:28 +0300

    navigation added

--- app/src/main/java/com/abdulkadirkara/filmverse/MainActivity.kt
+++ app/src/main/java/com/abdulkadirkara/filmverse/MainActivity.kt
@@ -11,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.abdulkadirkara.filmverse.presentation.navigation.Navigation
 import com.abdulkadirkara.filmverse.ui.theme.FilmVerseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FilmVerseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FilmVerseTheme {
-        Greeting("Android")
     }
 }
