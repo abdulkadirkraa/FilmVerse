abcdc0e9350b95e018a7abf17e3a003c74b080ac
abdulkadirkraa <karaabdulkadir498@gmail.com>
2025-01-03 00:31:41 +0300

    screens refactor

--- app/src/main/java/com/abdulkadirkara/filmverse/presentation/screens/screendetail/ScreenDetail.kt
+++ app/src/main/java/com/abdulkadirkara/filmverse/presentation/screens/screendetail/ScreenDetail.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowForwardIos
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Campaign
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.rounded.ArrowForwardIos
@@ -38,6 +39,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,6 +47,7 @@ import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.abdulkadirkara.data.remote.ApiConstants
 import com.abdulkadirkara.domain.model.FilmCardUI
+import com.abdulkadirkara.filmverse.R
 import com.abdulkadirkara.filmverse.presentation.screens.components.topappbar.DetailScreenCustomTopAppBar
 
 @Composable
@@ -55,75 +58,88 @@ fun ScreenDetail(film: FilmCardUI) {
         }
     ) { paddingValues ->
         val verticalState = rememberScrollState()
-
         Column(
             modifier = Modifier
                 .verticalScroll(verticalState)
                 .fillMaxWidth()
                 .padding(top = paddingValues.calculateTopPadding(), bottom = 16.dp),
         ) {
-            Box(
-                modifier = Modifier
-                    .size(48.dp) // Daha büyük bir arka plan
-                    .padding(8.dp) // Kartın köşelerinden uzaklaştırmak için padding ekledik
-                    .background(Color.LightGray, CircleShape)
-                    .align(Alignment.End)
-            ) {
-                Icon(
-                    Icons.Rounded.FavoriteBorder,
-                    contentDescription = "Favori",
-                    tint = if (film.isFavorite) Color.Red else Color.White,
+            Row (
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                val imageUrl = ApiConstants.IMAGE_BASE_URL + film.image
+                AsyncImage(
+                    model = imageUrl,
                     modifier = Modifier
-                        .fillMaxSize()
+                        .width(200.dp)
+                        .height(300.dp)
+                        .weight(50f),
+                    contentDescription = "Film Görseli",
+                    contentScale = ContentScale.FillHeight,
                 )
+                Column(
+                    modifier = Modifier.fillMaxWidth().padding(4.dp).weight(50f),
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp) // Daha büyük bir arka plan
+                            .padding(4.dp) // Kartın köşelerinden uzaklaştırmak için padding ekledik
+                            .background(Color.LightGray, CircleShape)
+                            .align(Alignment.End)
+                    ) {
+                        Icon(
+                            Icons.Rounded.FavoriteBorder,
+                            contentDescription = "Favori",
+                            tint = if (film.isFavorite) Color.Red else Color.White,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
+                    }
+                    Text(
+                        text = film.name,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 24.sp,
+                        color = Color.Black,
+                        modifier = Modifier.padding(bottom = 2.dp)
+                    )
+                    Text(
+                        text = film.category,
+                        fontSize = 20.sp,
+                        color = Color.Gray,
+                        modifier = Modifier.padding(bottom = 2.dp)
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            Icons.Filled.Star,
+                            contentDescription = "Rating",
+                            tint = Color.Yellow,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(text = film.rating.toString(), fontSize = 16.sp)
+                    }
+                    Text(
+                        text = film.director,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(bottom = 2.dp)
+                    )
+                    Text(
+                        text = film.description,
+                        color = Color.Gray,
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(bottom = 2.dp)
+                    )
+                }
             }
-            val imageUrl = ApiConstants.IMAGE_BASE_URL + film.image
-            AsyncImage(
-                model = imageUrl,
-                modifier = Modifier
-                    .width(400.dp)
-                    .height(600.dp),
-                contentDescription = "Film Görseli",
-                contentScale = ContentScale.FillHeight,
-            )
+
             HorizontalDivider(
                 color = Color.Gray,
                 thickness = 1.dp,
                 modifier = Modifier.padding(vertical = 4.dp)
             )
-            Text(
-                text = film.name,
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 24.sp,
-                color = Color.Black,
-                modifier = Modifier.padding(bottom = 2.dp)
-            )
-            Text(
-                text = film.category,
-                fontSize = 20.sp,
-                color = Color.Gray,
-                modifier = Modifier.padding(bottom = 2.dp)
-            )
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    Icons.Filled.Star,
-                    contentDescription = "Rating",
-                    tint = Color.Yellow,
-                    modifier = Modifier.size(24.dp)
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(text = film.rating.toString(), fontSize = 16.sp)
-            }
-            Text(
-                text = film.director,
-                fontSize = 20.sp,
-                modifier = Modifier.padding(bottom = 2.dp)
-            )
-            Text(
-                text = film.description,
-                fontSize = 20.sp,
-                modifier = Modifier.padding(bottom = 2.dp)
-            )
+
             Spacer(
                 modifier = Modifier
                     .height(16.dp)
@@ -222,6 +238,53 @@ fun ScreenDetail(film: FilmCardUI) {
                     .fillMaxWidth()
                     .background(Color.LightGray,)
             )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xFF0D47A1),
+                        contentColor = Color.White
+                    )
+                ) {
+                    Icon(
+                        painterResource(R.drawable.ic_minus_48),
+                        contentDescription = "minus",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "1",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xFF0D47A1),
+                        contentColor = Color.White
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "add",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+
+            }
             Row (
                 modifier = Modifier
                     .fillMaxWidth()
