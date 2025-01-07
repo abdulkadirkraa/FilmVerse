0ff91df8f9fdc6972bfcb921b1cc7995d90babf4
abdulkadirkraa <karaabdulkadir498@gmail.com>
2025-01-07 16:34:05 +0300

    swipeable tab rows added

--- app/src/main/java/com/abdulkadirkara/filmverse/presentation/screens/screenfavorites/ScreenFavourites.kt
+++ app/src/main/java/com/abdulkadirkara/filmverse/presentation/screens/screenfavorites/ScreenFavourites.kt
@@ -1,8 +1,103 @@
 package com.abdulkadirkara.filmverse.presentation.screens.screenfavorites
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.BookmarkBorder
+import androidx.compose.material.icons.filled.Collections
+import androidx.compose.material.icons.filled.CollectionsBookmark
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun ScreenFavorites(){
+fun ScreenFavorites() {
+    val tabItems = listOf(
+        TabItem(
+            title = "Favoriler",
+            unselectedIcon = Icons.Default.FavoriteBorder,
+            selectedIcon = Icons.Default.Favorite
+        ),
+        TabItem(
+            title = "Koleksiyonlar",
+            unselectedIcon = Icons.Default.BookmarkBorder,
+            selectedIcon = Icons.Default.Bookmark
+        )
+    )
 
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+
+    val pagerState = rememberPagerState(
+        pageCount = tabItems.size
+    )
+
+    LaunchedEffect(selectedTabIndex) {
+        pagerState.animateScrollToPage(selectedTabIndex)
+    }
+    LaunchedEffect(pagerState.currentPage, pagerState.isScrollInProgress) {
+        if (!pagerState.isScrollInProgress)
+            selectedTabIndex = pagerState.currentPage
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TabRow (selectedTabIndex = selectedTabIndex) {
+            tabItems.forEachIndexed { index, tabItem ->
+                Tab(
+                    selected = selectedTabIndex == index,
+                    onClick = { selectedTabIndex = index },
+                    text = { Text(text = tabItem.title) },
+                    icon = {
+                        Icon(
+                            imageVector = if (selectedTabIndex == index) tabItem.selectedIcon else tabItem.unselectedIcon,
+                            contentDescription = tabItem.title
+                        )
+                    }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth().weight(1f)
+        ) { index ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = tabItems[index].title)
+            }
+        }
+    }
 }
+
+
+data class TabItem(
+    val title: String,
+    val unselectedIcon: ImageVector,
+    val selectedIcon: ImageVector
+)
